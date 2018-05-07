4c0d1a8fcd3bc510a841494f499d1929cf14675b
justintsai <justin710836@gmail.com>
2018-05-07 12:42:35 +0800

    Rewrite check-and-update methods

--- PythonUploader.py
+++ PythonUploader.py
@@ -25,8 +25,9 @@ def get_data(url):
         for key, value in json_obj['payments'][i].items():
             data[i].append(value)
         data[i][0] = ' '.join(re.findall(pattern, data[i][0])[:-1]) # shorten timestamps
-    data.append(sum(map(float, list(map(list, zip(*data)))[1]))) # transpose "data" then sum payments
-    return data
+    # data.append(sum(map(float, list(map(list, zip(*data)))[1]))) # transpose "data" then sum payments
+    data.append(json_obj['totalPaid'])
+    return data # len(data) <= 31 since the json only stores the latest 30 payment
 
 def auth_gss_client(path, scope):
     credentials = ServiceAccountCredentials.from_json_keyfile_name(path, scope)
@@ -38,34 +39,30 @@ def add_sheet(gss_client, key, name, row, col):
     sheet = worksheet.worksheet(name)
     sheet.insert_row(['時間','數量','交易ID','狀態','總額'],1)
 
-def check(l, data, i):
-    data[i-1][1] = float(data[i-1][1])
-    l[i][1] = float(l[i][1])
-    return data[i-1] == l[i][:4] # l[0] is not part of payout history
-
-def look_into_sheet(l, data):
-    for i in range(1, len(data)): # data[len(data)-2] is the latest payout record
-        try:
-            check(l, data, i)
-        except:
-            return i-1 # (i-1) records on the sheet
-    return len(data)-1 # records are up to date
+def look_into_sheet(table, data):
+    # Return where the latest record in my sheet is in json data
+    # data[len(data)-2] is the latest payout record
+    for i in range(len(data)-1,0,-1):
+        data[i-1][1] = float(data[i-1][1])
+        table[-1][1] = float(table[-1][1])
+        if table[-1][:4] == data[i-1]:
+            return i-1 # if i-1 == 29, nothing to update
 
 def update_sheet(gss_client, key, data):
     worksheet = gss_client.open_by_key(key)
     sheet = worksheet.worksheet('出金記錄')
-    list_of_lists = sheet.get_all_values()
-    n_records = look_into_sheet(list_of_lists, data)
-    if n_records == len(data)-1:
+    table = sheet.get_all_values()
+    n_records = look_into_sheet(table, data)
+    if n_records == len(data)-2:
         print('Everything up-to-date')
     else:
-        print('%d record(s) not on the sheet!' % (len(data)-1 - n_records))
-        for i in range(n_records, len(data)-1):
-            sheet.insert_row(data[i], i+2)
+        print('%d record(s) not on the sheet!' % (len(data)-2 - n_records))
+        for i in range(1, len(data)-n_records-1):
+            sheet.insert_row(data[n_records+i], len(table)+i)
         sheet.update_acell('E2', data[len(data)-1])
 
 spreadsheetId = '1pAed6_SMO34Jet_BADdE6c2L8iiCMtFxnGNTJ2MSQCg' 
-# spreadsheetId = '1YVqHckcpI7BclVhTrh0wER3mNb1aNj7-65Xz59wjvRQ' for testing
+# spreadsheetId = '1YVqHckcpI7BclVhTrh0wER3mNb1aNj7-65Xz59wjvRQ' # for testing
 
 def main():
     sys.path = '/Users/Justin/Documents/Projects/ETH payout crawler/'
